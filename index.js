beeeb354aa9e45de126cc7aae800ae39239407a7
Adam Roddick <adam@Ads.local>
2018-11-18 16:38:15 +1100

    Add three different ways to use xhr incl buildQuery plugin from GMT

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const json = {
     1: 'Adam',
     2: 'Ben',
     3: 'Daniel',
-    4: 'Thomas' 
+    4: 'Thomas',
+}
+
+const json2 = {
+    1: 'Mariel',
+    2: 'Angie',
+    3: 'Amparo',
 }
 
 app.get('/', (req, res) => {
@@ -18,6 +24,15 @@ app.get('/', (req, res) => {
 app.get('/json', (req, res) => {
     res.send(json)
 })
+
+app.get('/json2', (req,res) => {
+    // res.send(input)
+    if (req.query.key == 'password123') {
+        res.send(json2)
+    } else {
+        res.send('Error: incorrect key')
+    }
+}) 
 
 app.listen(port, () => {
     console.log('Listening on port: '+ port)
