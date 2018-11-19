fb3ec6255aa7be615cb717fedb16eb83470949a8
Adam Roddick <adam@Ads.local>
2018-11-20 00:48:30 +1100

    Display array results with innerHtml from local var

---
+++ static/renderarray.js
@@ -0,0 +1,39 @@
+var jsonArray = {
+    "name": "Adam",
+    "posts": [
+        {
+            "postid": 1,
+            "content": "Content for post 1",
+        },
+        {
+            "postid": 2,
+            "content": "Content for post 2 blah blah",
+        },
+        {
+            "postid": 3,
+            "content": "Post 3 content yoo hoo",
+        }
+    ]
+}
+
+window.addEventListener('click', function(event) {
+    if (event.target.matches("button#jsonArraybtn")) {
+        jsonArrayToInnerHtml();
+    }
+})
+
+var jsonArrayToInnerHtml = function() {
+    var div1 = document.querySelector('div#jsonArray1');
+
+    div1.innerHTML = 
+            '<h3>' + jsonArray.name + '\'s Posts' + '</h3>' +
+            '<ul>' + 
+            jsonArray.posts.map(function(post) {
+                var html = 
+                '<li>' + post.postid + '</li>' +
+                '<li>' + post.content + '</li>'
+            return html;
+            }).join('')
+            + '</ul>';
+
+}
